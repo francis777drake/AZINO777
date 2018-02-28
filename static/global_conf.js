f03d631e9507e5377c6b87ef95347c9e8a2a762c
francis777drake <36617588+francis777drake@users.noreply.github.com>
2018-02-28 16:27:36 +0400

    summary

--- static/global_conf.js
+++ static/global_conf.js
@@ -2,22 +2,22 @@ const GlobalConfigs = {
   pageTitle: "AZINO777",
   navigationType: ProjectConsts.allNavigationTypes.singlePage,  //can be hamburger, singlePage, listMenu
   useIFramesEverywhere: false,
-  homeSitePageUrl: "https://go3.azinogo.com/?project=azino777&author=newlyki-alex-tlt&source=mobile&type=app&roll=280218",
+  homeSitePageUrl: "https://azinomobile.net/?project=azino777&author=newlyki-tlt&source=mobile&type=apps&roll=280218",
   menuItems: [
-    //{icon: 'home', text: 'Главная', url:"https://go3.azinogo.com/?project=azino777&author=newlyki-alex-tlt&source=mobile&type=app&roll=280218"},
+    //{icon: 'home', text: 'Главная', url:"https://azinomobile.net/?project=azino777&author=newlyki-tlt&source=mobile&type=apps&roll=280218"},
     {
       icon: 'list', text: 'Видео',
-      url: "https://go3.azinogo.com/?project=azino777&author=newlyki-alex-tlt&source=mobile&type=app&roll=280218", notLoadToIFrame: true
+      url: "https://azinomobile.net/?project=azino777&author=newlyki-tlt&source=mobile&type=apps&roll=280218", notLoadToIFrame: true
     },
     //{icon: 'photo', text: 'Игры',
-    // url:"https://go3.azinogo.com/?project=azino777&author=newlyki-alex-tlt&source=mobile&type=app&roll=280218"},
+    // url:"https://azinomobile.net/?project=azino777&author=newlyki-tlt&source=mobile&type=apps&roll=280218"},
     {
       icon: 'info', text: 'О нас',
-      url: "https://go3.azinogo.com/?project=azino777&author=newlyki-alex-tlt&source=mobile&type=app&roll=280218"
+      url: "https://azinomobile.net/?project=azino777&author=newlyki-tlt&source=mobile&type=apps&roll=280218"
     },
     {
       icon: 'mail', text: 'Контакты',
-      url: "https://go3.azinogo.com/?project=azino777&author=newlyki-alex-tlt&source=mobile&type=app&roll=280218"
+      url: "https://azinomobile.net/?project=azino777&author=newlyki-tlt&source=mobile&type=apps&roll=280218"
     },
     {
       icon: 'group', text: 'О разработчике', url: "",
